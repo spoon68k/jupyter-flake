bd84ec984a36b735a4b9e3f322fce77834b6dcc9
Dan Spooner <8625813+spoon68k@users.noreply.github.com>
2024-06-18 10:07:37 +0100

    Initial import

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+    nixpkgs-python.inputs = { nixpkgs.follows = "nixpkgs"; };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+
+                  # https://devenv.sh/reference/options/
+                  languages.python = {
+                    enable = true;
+                    version = "3.11.3";
+                    venv.enable = true;
+                    venv.requirements = ./requirements.txt;
+                  };
+
+                  packages = [
+                    pkgs.parquet-tools
+                  ];
+
+                  enterShell = ''
+jupyter notebook --no-browser --port 9999 --notebook-dir=notebooks --NotebookApp.token=""
+                  '';
+                }
+              ];
+            };
+          });
+    };
+}
